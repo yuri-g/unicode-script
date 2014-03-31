bb2de482176502ddca49c6f657a40c3510724284
Yuri <yuri.goncharenko@gmail.com>
2014-03-31 16:07:15 +0200

    add support for multi-word in method_missing

--- lib/unicode_script/core.rb
+++ lib/unicode_script/core.rb
@@ -1,46 +1,46 @@
 module UnicodeScript
 
-    def self.detect string
-      res = []
-      string.tr!(' ','')
-      string.codepoints.each do |c|
-        script = find_script(c)
-        index = res.find_index{|v| v[:script] == script}
-        if script
-          if index
-            res[index][:value].push(c.chr)
-          else
-            res.push({:script => script, :value => [].push(c.chr)})
-          end
+  def self.detect(string)
+    res = []
+    string.tr!(' ','')
+    string.codepoints.each do |c|
+      script = find_script(c)
+      index = res.find_index{|v| v[:script] == script}
+      if script
+        if index
+          res[index][:value].push(c.chr)
+        else
+          res.push({:script => script, :value => [].push(c.chr)})
         end
+      end
 
-      end  
-      res.each do |r| 
-        r[:value] = r[:value].join('')
-      end
-      res
+    end  
+    res.each do |r| 
+      r[:value] = r[:value].join('')
     end
-  
-  
-    def self.method_missing method, val
-      CHARTS.each do |c|
-        if c[:name].downcase == method.to_s.chop
-          val.codepoints.each do |p|
-            return false if !(c[:range].include?(p)) 
-          end
-          return true
+    res
+  end
+
+
+  def self.method_missing(method, val)
+    CHARTS.each do |c|
+      if c[:name].downcase == method.to_s.gsub('_', ' ').chop
+        val.codepoints.each do |p|
+          return false if !(c[:range].include?(p)) 
         end
+        return true
       end
-      super
     end
-  
-    private
-  
-    def self.find_script codepoint
-      CHARTS.each do |c|
-        return c[:name] if c[:range].include? codepoint
-      end
-      nil
+    super
+  end
+
+  private
+
+  def self.find_script(codepoint)
+    CHARTS.each do |c|
+      return c[:name] if c[:range].include? codepoint
     end
+    nil
+  end
 
 end
--- spec/lib/unicode_script_spec.rb
+++ spec/lib/unicode_script_spec.rb
@@ -9,13 +9,13 @@ describe 'UnicodeScript' do
     UnicodeScript.detect(h).should eq([{:script => 'Hiragana', :value => 'ひらがな'}])
     UnicodeScript.detect(k).should eq([{:script => 'Katakana', :value => 'カタカナ'}])
     UnicodeScript.detect(mixed).should eq([{:script => 'CJK Unified Ideographs', :value => '東京'},
-                                          {:script => 'Basic Latin', :value => 'Tokyo'}])
+                                           {:script => 'Basic Latin', :value => 'Tokyo'}])
   end
-  
+
   it 'should be able to check whether string belongs to certain script' do
-    h = 'ひらがな'
+    h = '漢字'
     mixed = 'ひらaaaがな'
-    UnicodeScript.hiragana?(h).should eq(true)
+    UnicodeScript.cjk_unified_ideographs?(h).should eq(true)
     UnicodeScript.hiragana?(mixed).should eq(false)
   end
 end 
