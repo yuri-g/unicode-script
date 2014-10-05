031d2aac94a37a3b60ac275c68069f381cbf33e2
yurigg <yuri.goncharenko@gmail.com>
2014-10-05 13:42:28 +0300

    Update spec
    Remove should matchers

--- spec/lib/unicode_script_spec.rb
+++ spec/lib/unicode_script_spec.rb
@@ -6,16 +6,16 @@ describe 'UnicodeScript' do
     h = 'ひらがな'
     k = 'カタカナ'
     mixed = '東京 Tokyo'
-    UnicodeScript.detect(h).should eq([{ script:  'hiragana', value: 'ひらがな' }])
-    UnicodeScript.detect(k).should eq([{ script:  'katakana', value:  'カタカナ' }])
-    UnicodeScript.detect(mixed).should eq([{ script:  'cjk unified ideographs', value: '東京' },
+    expect(UnicodeScript.detect(h)).to eq([{ script:  'hiragana', value: 'ひらがな' }])
+    expect(UnicodeScript.detect(k)).to eq([{ script:  'katakana', value:  'カタカナ' }])
+    expect(UnicodeScript.detect(mixed)).to eq([{ script:  'cjk unified ideographs', value: '東京' },
                                            { script:  'basic latin', value: 'Tokyo' }])
   end
 
   it 'should be able to check whether string belongs to certain script' do
     h = '漢字'
     mixed = 'ひらaaaがな'
-    UnicodeScript.cjk_unified_ideographs?(h).should eq(true)
-    UnicodeScript.hiragana?(mixed).should eq(false)
+    expect(UnicodeScript.cjk_unified_ideographs?(h)).to eq(true)
+    expect(UnicodeScript.hiragana?(mixed)).to eq(false)
   end
 end
